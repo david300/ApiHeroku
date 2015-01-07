33585d266ed7331e1fab23ffdfc3dd5aaa6365b6
david300 <davidferreira87@gmail.com>
2015-01-07 00:32:43 -0300

    KJHKJH

--- controllers/chats.js
+++ controllers/chats.js
@@ -1,5 +1,5 @@
-var mongoose = require('mongoose');
-var chat  = mongoose.model('Chat');
+//var mongoose = require('mongoose');
+//var chat  = mongoose.model('Chat');
 
 //GET - Return all tvshows in the DB
 exports.findAllChats = function(req, res) {
@@ -10,11 +10,39 @@ exports.findAllChats = function(req, res) {
 
     console.log("Traeme todos");
 
-    chat.find(function(err, chats) {
-        if(!err) {
-            res.send(chats);
-        } else {
-            console.log('ERROR: ' + err);
+    var chatsR = 
+    [
+        {
+            "_id":"549c75400192a4cf8fdbbb08",
+            "name":"Ben Sparrow",
+            "lastText":"You on your way?",
+            "face":"https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png"
+        },
+        {
+            "_id":"549c75540192a4cf8fdbbb0a",
+            "name":"Andrew Jostlin",
+            "lastText":"Did you get the ice cream?",
+            "face":"https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg"
+        },
+        {
+            "_id":"549c755b0192a4cf8fdbbb0b",
+            "name":"Adam Bradleyson",
+            "lastText":"I should buy a boat",
+            "face":"https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg"
+        },
+        {
+            "_id":"549c75620192a4cf8fdbbb0c",
+            "name":"Perry Governor",
+            "lastText":"Look at my mukluks!",
+            "face":"https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg"
         }
-    });
+    ];
+    res.send(chatsR);
+//    chat.find(function(err, chats) {
+//        if(!err) {
+//            res.send(chats);
+//        } else {
+//            console.log('ERROR: ' + err);
+//        }
+//    });
 }
